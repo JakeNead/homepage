45ad34502a5393b31fa415a72fd4a3fb7522456d
Jake Nead <jakenead@gmail.com>
2024-01-07 21:25:56 -0800

    Complete mobile layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-// import "./style.css";
-
-const body = document.querySelector("body");
-
-const div = document.createElement("div");
-div.textContent = "This is a div.";
-
-body.appendChild(div);
+import "./style.css";
